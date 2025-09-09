9f6957aaf59052b219ca665082b65c8d4d77a239
oabnerwilliam <abnerwilliam026@gmail.com>
2025-09-09 15:13:49 -0300

    adicionando página no app

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
-import "./App.css";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { Produtos } from "./components/Produtos";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Produtos } from "./screens/produtos/Produtos"
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Produtos />
     </QueryClientProvider>
-  );
+  )
 }
 
-export default App;
+export default App
